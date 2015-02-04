0986dccf49d93d42174401aaab332a6c4260537b
MrCochese <rickbolton@freyjapterssmbp.home>
2015-02-04 23:30:02 +0000

    Universal messaging
    Naive implementation of non-IRC universal messaging just to get
    something working. Start to break into components and event emitters
    next.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 var net = require('net');
 var messageReader = require('./messageReader.js')
 
+var connections = [];
+
 var server = net.createServer(function(connection) {
 
 	var nick = null;
 	var registered = false;
+	var user = null;
 
 	var address = connection.remoteAddress;
 
@@ -14,6 +17,11 @@ var server = net.createServer(function(connection) {
 
 	connection.on('end', function() {
 		console.log('connection from ' + address + ' closed\n');
+
+		var index = connections.indexOf(connection);
+		if (index > -1) {
+			connections.splice(index, 1);
+		}
 	});
 
 	var commandProcessor = function(message) {
@@ -21,14 +29,44 @@ var server = net.createServer(function(connection) {
 
 		console.log(command);
 
-		if (command && command.length > 1 && command[0] === 'NICK') {
-			registered = true;
-			nick = command[1];
-			connection.write('Nick changed to ' + nick + '\n');
+		if (command && command.length > 1) {
+			if (command[0] === 'NICK') {
+				nick = command[1];
+				connection.write('Nick changed to ' + nick + '\n');
+			}
+			else if (command[0] === 'USER') {
+				registered = true;
+				user = command[1];
+
+				if (!nick) {
+					nick = user;
+				}
+
+				connections.push(connection);
+
+				connection.write('User registered: ' + user + '\n');
+			}
+			else if (command[0] === 'MESSAGE') {
+				if (!registered)
+				{
+					connection.write('User not registered');
+					return;
+				}
+
+				var delimited = message.match(/"(.*)"/g);
+				console.log(delimited);
+
+				for (var i = 0; i < connections.length; i++) {
+					console.log(nick + ': ' + delimited[0]);
+					connections[i].write(nick + ': ' + delimited[0] + '\n');
+				}
+			}
 		}
-		else if (!registered)
-		{
+		else if (!nick) {
 			connection.write('Use NICK to set user name\n');
+		}
+		else if (!registered) {
+			connection.write('Use USER to register user\n');
 		}
 		else
 		{
